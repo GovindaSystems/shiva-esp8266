fc003a51a146683ca6a026793b44c5cb145cdad9
Govinda <govinda777@gmail.com>
2023-03-05 12:59:03 -0300

    up

--- src/iot.sensor/main.py
+++ src/iot.sensor/main.py
@@ -3,11 +3,15 @@
 
 import time
 import sys
-from infra.mqtt import Mqtt
-from infra.wifi import Wifi
+from mqtt import Mqtt
+from wifi import Wifi
 
-WIFI_SSID = "VIVOFIBRA-6CA1"
-WIFI_PW = "Pedro2203"
+WIFI_SSID = "ACADEMIA BOULDER"
+WIFI_PW = "boulderv10"
+
+# Local
+# WIFI_SSID = "VIVOFIBRA-6CA1"
+# WIFI_PW = "Pedro2203"
 mqtt = Mqtt()
 
 #start execution
@@ -30,7 +34,7 @@ except Exception as e:
 while True: 
     try: 
         time.sleep(2)
-        #mqtt.wait_msg() #blocking 
+        
         mqtt.check_msg() #non-blocking 
          
     except KeyboardInterrupt: 
